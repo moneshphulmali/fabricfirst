1cd0ab1440519c00ccfc83239cf22dc38f0c8c15
moneshphulmali <moneshmca24@ipsacademy.org>
2026-05-29 15:08:13 +0530

    chahge the filter lout in btoc page

--- dash.php
+++ dash.php
@@ -136,7 +136,7 @@ $storeid = $current_store_id;
   <label><strong>Select Date:</strong></label>
   <input type="date" id="filterDate" onchange="filterByDate()">
   
-  <label><strong>Search:</strong></label>
+  <label><strong>Searchhh:</strong></label>
   <input type="text" id="searchInput" onkeyup="searchOrders()" placeholder="Order ID, Name, or Phone">
 </div>
 
